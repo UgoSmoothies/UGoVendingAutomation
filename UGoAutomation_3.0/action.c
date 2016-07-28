7266f8f7752eb4716cfadbdfeb2ab003fe0052c2
sapiah <sachinappiah20@gmail.com>
2016-07-28 10:54:02 -0400

    Changes to the blend sequence

--- UGoAutomation_3.0/action.c
+++ UGoAutomation_3.0/action.c
@@ -26,7 +26,7 @@ void blend_actions_init() {
   // [3-6] Lower and wait, lower and wait, lower and wait
   for (j = 0; j < 4; i++) {
     blend_sequence.actions_ptr[i++].type = ACTION_MTP;
-    blend_sequence.actions_ptr[i].mtp.new_position = TOP_OF_CUP + (5 + j) + 5; // position
+    blend_sequence.actions_ptr[i].mtp.new_position = TOP_OF_CUP + (5 * j) + 5; // position
     blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_DOWN;
     blend_sequence.actions_ptr[i].mtp.new_position = MOTOR_SPEED_HALF;
 
@@ -34,8 +34,27 @@ void blend_actions_init() {
     blend_sequence.actions_ptr[i].wait.time_to_wait = 1000; //ms
   }
 
-  // [7-22] Up and Down twice, not quite to top of cup
-  for (j = 0; j < 15; i++) {
+   // [7-8] Lower and wait, Raise and wait
+  for(j=0; j<2; j++)  {
+    blend_sequence.actions_ptr[i++].type = ACTION_MTP;
+    blend_sequence.actions_ptr[i].mtp.new_position = BOTTOM_OF_CUP; // position
+    blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_DOWN;
+    blend_sequence.actions_ptr[i].mtp.new_position = MOTOR_SPEED_HALF;
+
+    blend_sequence.actions_ptr[i++].type = ACTION_WAIT;
+    blend_sequence.actions_ptr[i].wait.time_to_wait = 500; //ms
+
+    blend_sequence.actions_ptr[i++].type = ACTION_MTP;
+    blend_sequence.actions_ptr[i].mtp.new_position = TOP_OF_SMOOTHIE + 10; // position
+    blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_UP;
+    blend_sequence.actions_ptr[i].mtp.new_position = MOTOR_SPEED_HALF;
+
+    blend_sequence.actions_ptr[i++].type = ACTION_WAIT;
+    blend_sequence.actions_ptr[i].wait.time_to_wait = 500; //ms
+  }
+
+  // [9-16] Up and Down twice, not quite to top of cup
+  for (j = 0; j < 8; i++) {
     blend_sequence.actions_ptr[i++].type = ACTION_MTP;
     blend_sequence.actions_ptr[i].mtp.new_position = BOTTOM_OF_CUP; // position
     blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_DOWN;
@@ -53,18 +72,18 @@ void blend_actions_init() {
     blend_sequence.actions_ptr[i].wait.time_to_wait = 500; //ms
   }
 
-  // 23. Move to top, stay in liquid
+  // 17. Move to top, stay in liquid
   blend_sequence.actions_ptr[i++].type = ACTION_MTP;
   blend_sequence.actions_ptr[i].mtp.new_position = TOP_OF_CUP - 20; // position
   blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_UP;
   blend_sequence.actions_ptr[i].mtp.new_position = MOTOR_SPEED_HALF;
 
-  // 24. Turn blender off
+  // 18. Turn blender off
   blend_sequence.actions_ptr[i++].type = ACTION_ACTIVATE;
   blend_sequence.actions_ptr[i].activate.address = BLENDER_ADDRESS;
   blend_sequence.actions_ptr[i].activate.state = OFF;
 
-  // 25. Return home
+  // 19. Return home
   blend_sequence.actions_ptr[i++].type = ACTION_MTP;
   blend_sequence.actions_ptr[i].mtp.new_position = TOP_POSITION; // position
   blend_sequence.actions_ptr[i].mtp.move_direction = BLENDER_MOVEMENT_UP;
